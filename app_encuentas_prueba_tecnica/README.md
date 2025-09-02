36a83e23a18af1940f155ddddfc9dd088a98e6ca
eliasDniel <eramosg2@unemi.edu.ec>
2025-09-01 19:26:10 -0500

    Frontend Flutter + Backend Django API

---
+++ app_encuentas_prueba_tecnica/README.md
@@ -0,0 +1,3 @@
+# app_encuentas_prueba_tecnica
+
+A new Flutter project.
---
+++ app_encuentas_prueba_tecnica/android/app/src/main/kotlin/com/example/app_encuentas_prueba_tecnica/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_encuentas_prueba_tecnica
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
